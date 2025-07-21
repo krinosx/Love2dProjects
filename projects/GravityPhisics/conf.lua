4a2da2e713ca6b42ad75b398f45aced8d67acc8c
Krinosx <krinosx@gmail.com>
2025-07-21 21:18:02 +0200

    Adding some extra ideas for force/angles
    Splitting forces in X and Y components
    
    Still need to understand a better (mor inteuitive) way to represent forces
    
    At least now we have positive forces ponting towards the "sun"  and negative forces pointing towards the planet.
    
    Next step is to work how to propper draw the arrows representing the gravity pull

--- projects/GravityPhisics/conf.lua
+++ projects/GravityPhisics/conf.lua
@@ -1,5 +1,5 @@
 function love.conf(t)
     t.window.title = "GravityPhisics"
-    t.window.width = 2000
-    t.window.height = 1200
+    t.window.width = 1800
+    t.window.height = 920
 end
--- projects/GravityPhisics/gravity.lua
+++ projects/GravityPhisics/gravity.lua
@@ -4,22 +4,42 @@ local Gravity = {
 
 CelestialBody = require("CelestialBody")
 
+
 Gravity.localInfluence = function(self, x, y, x1, y1, mass)
   -- General formula of gravity
   --  F = G * (m1 * m2) / d²,
   -- on our case we will consider m2 as a point without mass
 
+  local distVec = self.distanceVector(x,y,x1,y1)
+
   local distance = self.distance(x, y, x1, y1);
   local Force = self.G * mass / (distance ^ 2)
 
-  local angle = math.atan2(y - y1, x - x1)
 
 
-  return Force, angle
+  local ForceX = 0
+  if distVec.x > 0 then
+   ForceX = self.G * mass / (distVec.x^2)
+  end
+
+  local ForceY = 0
+  if distVec.y > 0 then
+    ForceY = self.G * mass / (distVec.y^2)
+  end
+
+  local angle = math.atan2(y1 - y, x1 - x)
+
+
+  return {Force=Force, angle=angle, ForceX=ForceX, ForceY=ForceY, distVec=distVec}
 end
 
 Gravity.distance = function(x, y, x1, y1)
   return math.sqrt((x - x1) ^ 2 + (y - y1) ^ 2)
+end
+
+
+Gravity.distanceVector = function(x, y, x1, y1)
+  return { x=math.abs(x-x1), y=math.abs(y-y1) }
 end
 
 ---
@@ -50,11 +70,24 @@ Gravity.drawFieldGrid = function(gridW, gridH, cellSize, body, body2)
       -- love.graphics.rectangle("line",loc_x,loc_y, cellSize, cellSize)
 
 
-      local force, angle = Gravity:localInfluence(loc_x, loc_y, bodyX, bodyY, 1000)
-      local force1, angle1 = Gravity:localInfluence(loc_x, loc_y, body2x, body2Y, 500)
+      --local force, angle = Gravity:localInfluence(loc_x, loc_y, bodyX, bodyY, 1000)
+      --local force1, angle1 = Gravity:localInfluence(loc_x, loc_y, body2x, body2Y, 500)
 
-      force = force - force1
-      angle = angle - angle1
+      local influence = Gravity:localInfluence(loc_x, loc_y, bodyX, bodyY, 1000)
+      local influence_p1 = Gravity:localInfluence(loc_x, loc_y, body2x, body2Y, 300)
+
+      local force = influence.Force
+      local angle = influence.angle
+
+      local force_p1 = influence_p1.Force
+      local angle_p1 = influence_p1.angle
+
+
+      force = force - force_p1
+      --angle = angle - angle1
+
+      local force_x = influence.ForceX - influence_p1.ForceX
+      local force_y = influence.ForceY - influence_p1.ForceY
 
 
       local r,g,b,a = love.graphics.getColor()
@@ -74,16 +107,16 @@ Gravity.drawFieldGrid = function(gridW, gridH, cellSize, body, body2)
       end
       
 
-      love.graphics.push()
-      love.graphics.translate(loc_x, loc_y);
-      love.graphics.rotate(angle)
-      love.graphics.line(0, 0, 30, 0)
-      love.graphics.circle("fill", 0, 0, 4);
-      love.graphics.pop()
+      -- love.graphics.push()
+      -- love.graphics.translate(loc_x, loc_y);
+      -- love.graphics.rotate(angle)
+      -- love.graphics.line(0, 0, 30, 0)
+      -- love.graphics.circle("fill", 0, 0, 4);
+      -- love.graphics.pop()
 
-
-
-      love.graphics.print(string.format("%.3f", force), loc_x, loc_y)
+      love.graphics.print(string.format("x:%.3f\ny:%.3f", force_x, force_y), loc_x, loc_y)
+      --love.graphics.print(string.format("(%d,%d)", loc_x, loc_y), loc_x, loc_y)
+      --love.graphics.print(string.format("%.3f", force), loc_x, loc_y)
       --love.graphics.print(string.format("%.3f", angle), loc_x, loc_y + 15)
       --love.graphics.print(string.format("%.3f", angle1), loc_x, loc_y + 25)
       love.graphics.setColor(r,g,b,a)
